60de11af02fe2575b43a4d42785787560e3b8a2e
Antonio Murcia <amurciaa61@gmail.com>
2020-10-09 13:17:01 +0200

    Creo e implemento los metodos de los eventos MouseEnter y MouseLeave, incluyo las imagenes

--- 6_Opacidad/MainWindow.xaml.cs
+++ 6_Opacidad/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace _6_Opacidad
 {
@@ -20,9 +10,22 @@ namespace _6_Opacidad
     /// </summary>
     public partial class MainWindow : Window
     {
+        Image imagen;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imagen = sender as Image;
+            imagen.Opacity = 1;
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imagen = sender as Image;
+            imagen.Opacity = 0.5;
         }
     }
 }
